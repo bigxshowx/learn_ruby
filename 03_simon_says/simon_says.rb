af91995a3fc0e383246fb66b107482eddd1cb663
bigxshowx <bigxshowx@gmail.com>
2017-04-23 13:05:18 +0900

    Slowly but steady will complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
-#write your code here
+def echo(word)
+	return word
+end
+
+def shout(word)
+	return word.upcase
+end
+
+def repeat(word, x=nil)
+ if x==nil
+ 	return "#{word} #{word}"
+ else
+   arr = []
+    x.times do
+      arr.push(word)
+    end
+    return arr.join(' ')
+ end
+end
+
+def start_of_word(x,i)
+	x[0...i]
+end
+
+def first_word(x)
+	x.split(' ')[0]
+end
+
+def titleize(x)
+  arr = []
+	x.split(' ').each do |x| 
+      arr.push(x)
+		end
+	string = arr.map{|word| word.capitalize}.join(' ')
+	string.gsub!('And', 'and')
+	string.gsub!('The', 'the')
+	string.sub!('the', 'The')
+	string
+end 
